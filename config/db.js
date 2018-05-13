60157bcdc6ae24d6e22cd5945ea4ac74ecfa689f
Siddhant Sinha <sid.sinha94@gmail.com>
2018-05-13 13:38:57 +0530

    add production config

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost/jwtauth');
-const mongoDb = mongoose.connect('mongodb://127.0.0.1:27017/jwtauth', (err, db) => {
+// const mongoDb = mongoose.connect('mongodb://127.0.0.1:27017/jwtauth', (err, db) => {
+const mongoDb = mongoose.connect('mongodb://admin:aviana_admin@ds119080.mlab.com:19080/avianadb', (err, db) => {
   if (err) {
     console.log('Unable to connect to the server. Please start the server. Error:', err);
   } else {
-    console.log('Connected to Server successfully!');
+    console.log('Mongoose default connection open to prod url');
   }
 });
 module.exports = mongoDb;
